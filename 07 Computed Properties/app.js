f91fa8735a730432207e21ba0ec7f1b46485521d
Gabriel Vidal <gabrieldkvl@icloud.com>
2026-03-16 10:27:35 +0800

    Module 1 done

--- 07 Computed Properties/app.js
+++ 07 Computed Properties/app.js
@@ -8,16 +8,19 @@ const app = Vue.createApp({
           title: "Name of the Wind",
           author: "Patrick Rothfuss",
           img: "assets/1.jpg",
+          isFav: true,
         },
         {
           title: "The Way of Kings",
           author: "Brandon Sanderson",
           img: "assets/2.jpg",
+          isFav: false,
         },
         {
           title: "The Final Empire",
           author: "Brandon Sanderson",
           img: "assets/3.jpg",
+          isFav: true,
         },
       ],
     };
@@ -25,6 +28,14 @@ const app = Vue.createApp({
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
