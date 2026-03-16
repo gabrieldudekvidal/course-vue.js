89cdac3a9fab15ef365e16aa07afa62029d71115
Gabriel Vidal <gabrieldkvl@icloud.com>
2026-03-16 13:38:29 +0800

    Learning section 3...

---
+++ 08 Modal Project/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <!-- Using the component and props to pass data. @close is activated by the emit event on Modal.vue -->
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">Show modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal";
+
+export default {
+  name: "App",
+  components: { Modal } /* Using the modal component */,
+  data() {
+    return {
+      title: "My First Vue App",
+      header: "Sign up for the Giveaway!",
+      text: "Grab your discount.",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
---
+++ 08 Modal Project/src/components/Modal.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="backdrop" @click="closeModal">
+    <!-- If the value of theme is sale add the sale class -->
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <!-- Using the data passed from the prop -->
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"] /* Setting the prop */,
+  methods: {
+    closeModal() {
+      /* Emit an event to the App.vue */
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Use scoped style for specific components */
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: #fff;
+  border-radius: 10px;
+}
+
+.backdrop {
+  top: 0;
+  left: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
+}
+
+.sale {
+  background-color: crimson;
+  color: #fff;
+}
+
+.sale h1 {
+  color: #fff;
+}
+</style>
