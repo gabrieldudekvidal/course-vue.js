87b97e1e1a4a156c36bc2f1bfb848c1c57077fdb
Gabriel Vidal <gabrieldkvl@icloud.com>
2026-03-17 21:45:54 +0800

    Learning forms

---
+++ 10 Forms/src/components/SignupForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <form>
+    <label>Email</label>
+    <!--- Using v-model for data binding -->
+    <input type="email" required v-model="email" />
+
+    <label>Password</label>
+    <input type="password" required v-model="password" />
+
+    <label>Role</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+  </form>
+
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    // Data function
+    return {
+      // Returns an object
+      email: "",
+      password: "",
+      role: "",
+    };
+  },
+};
+</script>
+
+<style>
+label {
+  display: inline-block;
+}
+</style>
